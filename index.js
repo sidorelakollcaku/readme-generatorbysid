081fc6602b753b74f73f621cd94684f46002946e
sidorela Kollcaku <sidorelakollcaku@gmail.com>
2020-08-07 15:17:31 -0400

    index.js question and input created

---
+++ index.js
@@ -0,0 +1,186 @@
+let inquirer = require('inquirer');
+let fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What's your project title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Describe your projecj:",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Add installation insructions with separated list:",
+            name: "install"
+        },
+        {
+            type: "input",
+            message: "Add usage info for your project:",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Enter contribution guidelines for your project:",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Enter test information for your project:",
+            name: "testing"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub Username:",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Licence for the project?",
+            name: "license",
+            choices: [
+                "Apache License 2.0",
+                "GNU General Public License v3.0",
+                "MIT License",
+                "BSD 2-Clause License",
+                "BSD 3-Clause License",
+                "Boosy Software License 1.0",
+                "Creative Commons Zero v1.0 Universal",
+                "Eclipse Public License 2.0",
+                "GNU Affero General Public License v3.0",
+                "GNU Affero Public License v2.0",
+                "GNU Lesser General Public License v2.1",
+                "Mozilla Public License 2.0",
+                "The Unilicense"
+            ]
+        }
+    ])
+    .then((res) => {
+        console.log("Creating README file...");
+        createREADMEFile(res);
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    
+    
+function createREADMEFile(input) {
+    let readmeTitle;
+    let readmeDescription;
+    const descriptionHead = "## Description";
+    let tableOfContents;
+    const tocHead = "## Table of Contents";
+    let installArr;
+    const installHead = "## Installation";
+    let readmeUsage;
+    const usageHead = "## Usage";
+    let readmeContribution;
+    const contributionHead = "## Contribution";
+    let readmeTest;
+    const testingHead = "## Tests";
+    let readmeLicence = input.license;
+    const licenseHead = "## License";
+    let readmeQuestions;
+    const questionsHead = "## Questions";
+    let completeREADME = [];
+    
+    // Title
+    if (input.title == '') {
+        readmeTitle = '# TITLE';
+    } else {
+        readmeTitle = `# ${input.title}`;
+    }
+    completeREADME.push(readmeTitle);
+    
+    
+    // license badge !!
+    let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
+    completeREADME.push(badge);
+    
+    
+    // description
+    if (input.description == '') {
+        readmeDescription = `${descriptionHead}\n Enter project description here.`;
+    } else {
+        readmeDescription = `${descriptionHead}\n${input.description}`;
+    }
+    completeREADME.push(readmeDescription);
+    
+    
+    // Table of Contents
+    tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+    completeREADME.push(tableOfContents);
+    
+    
+    //installation instructions
+    completeREADME.push(`${installHead}`);
+    
+    installArr = input.install.split(',').map(item => {
+        return `${item.trim()}`;
+    });
+    
+    for (var i = 0; i < installArr.length; i++) {
+        completeREADME.push(`${i + 1}. ${installArr[i]}`);
+    }
+    
+    
+    // Usage
+    if (input.usage == '') {
+        readmeUsage = `\n${usageHead}\n Enter project usage here.`;
+    } else {
+        readmeUsage = `\n${usageHead}\n${input.usage}`;
+    }
+    completeREADME.push(readmeUsage);
+    
+    
+    // Contributing
+    if (input.contribution == '') {
+        readmeContribution = `\n${contributionHead}\n Enter project contriburtion information here.`;
+    } else {
+        readmeContribution = `\n${contributionHead}\n${input.contribution}`;
+    }
+    completeREADME.push(readmeContribution);
+    
+    
+    //Adding Tests
+    if (input.testing == '') {
+        readmeTest = `\n${testingHead}\n Enter project testing information here.`;
+    } else {
+        readmeTest = `\n${testingHead}\n${input.testing}`;
+    }
+    completeREADME.push(readmeTest);
+    
+    
+    //License info
+    readmeLicence = `\n${licenseHead}\nThis project is convered under the ${input.license}.`;
+    completeREADME.push(readmeLicence);
+    
+    
+    //Questions section with github link
+    readmeQuestions = `\n${questionsHead}\nFor questions about this project, please see my GitHub at [${input.github}](https://github.com/${input.github}),or email me at ${input.email}.`;
+    completeREADME.push(readmeQuestions);
+    
+    
+    
+    const README = completeREADME.join('\n');
+        
+    
+    //Creating the README
+    fs.writeFile("./example/README-example.md", README, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("README file successfully created!");
+        }
+    });
+}
